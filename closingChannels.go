4f2fe364f38adbb34c677ec5bcbf523d1b1f30cf
Oliver Mueller <oliver.mueller@esrlabs.com>
2015-05-02 07:59:16 +0200

    discarding duplicates

--- closingChannels.go
+++ closingChannels.go
@@ -8,7 +8,7 @@ import (
 
 const WORKERS int = 5
 
-func crunchNumber(queue chan int, quit <-chan bool, id int, wg *sync.WaitGroup, workerWg *sync.WaitGroup) {
+func crunchNumber(queue <-chan int, out chan<- int, quit <-chan bool, id int, wg *sync.WaitGroup, workerWg *sync.WaitGroup) {
 	defer workerWg.Done()
 	for {
 		select {
@@ -20,8 +20,8 @@ func crunchNumber(queue chan int, quit <-chan bool, id int, wg *sync.WaitGroup, 
 			if task > 1 {
 				res := task / 2
 				rest := task - res
-				queue <- res
-				queue <- rest
+				out <- res
+				out <- rest
 				wg.Add(2)
 			} else {
 				fmt.Printf("[%d]:done processing\n", id)
@@ -37,6 +37,7 @@ func crunchNumber(queue chan int, quit <-chan bool, id int, wg *sync.WaitGroup, 
 
 func main() {
 	queue := make(chan int, 100)
+	unfiltered := make(chan int, 100)
 	quit := make(chan bool)
 	var wg sync.WaitGroup
 	var workerWg sync.WaitGroup
@@ -46,8 +47,10 @@ func main() {
 
 	workerWg.Add(WORKERS)
 	for i := 0; i < WORKERS; i++ {
-		go crunchNumber(queue, quit, i, &wg, &workerWg)
+		go crunchNumber(queue, unfiltered, quit, i, &wg, &workerWg)
 	}
+	// setup filtering
+	go filterDuplicates(unfiltered, queue, &wg)
 
 	fmt.Printf("waiting for queue...\n")
 	wg.Wait()
@@ -58,4 +61,17 @@ func main() {
 	fmt.Printf("waiting for crunchers...\n")
 	workerWg.Wait()
 	fmt.Printf("crunchers done!!\n")
+}
+
+func filterDuplicates(in chan int, out chan int, wg *sync.WaitGroup) {
+	var seen = make(map[int]bool)
+	for v := range in {
+		if !seen[v] {
+			seen[v] = true
+			out <- v
+		} else {
+			fmt.Printf("discarding %d!!\n", v)
+			wg.Done() //already counted for, but we don't use it
+		}
+	}
 }
